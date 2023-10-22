52dec62a28b535d533d96a24672cbb4bd87c46a9
myesn <myesn@foxmail.com>
2023-10-22 23:52:12 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,17 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// https://nextjs.org/docs/app/api-reference/functions/generate-metadata
 export const metadata: Metadata = {
   title: 'cinema records',
   description: 'myEsn2E9\'s cinema records',
+  // 非常重要，不设置会导致出现横向滚动条
+  viewport: {
+    width: 'device-width',
+    minimumScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
 }
 
 export default function RootLayout({
