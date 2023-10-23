d2d75725a4f6ffa6144692ad67d841e4ea2c124e
myesn <myesn@foxmail.com>
2023-10-23 09:09:06 +0000

    refactor: beautify

--- components/CinemaListItem.tsx
+++ components/CinemaListItem.tsx
@@ -2,10 +2,10 @@ import { CinemaDto } from "@/types/dto";
 
 export default function CinemaListItem(props: ItemProps) {
   return (
-    <div className="flex">
-      <p color="violet" className="flex-1 truncate">{props.name}</p>
+    <div className="flex space-x-2">
+      <p color="violet" className="flex-1 font-medium truncate">{props.name}</p>
       <p className="flex-1 truncate">{props.remarks}</p>
-      <p className="flex-1">{props.updated}</p>
+      <p className="flex-none text-right">{props.updated}</p>
     </div>
   );
 }
