982b7a2436017754164a7154dbdf1d22f239ee9b
myesn <myesn@foxmail.com>
2023-10-23 04:03:39 +0000

    beautify list item

--- components/CinemaListItem.tsx
+++ components/CinemaListItem.tsx
@@ -4,11 +4,11 @@ import { Text, Bold, Flex } from "@tremor/react";
 export default function CinemaListItem(props: ItemProps) {
   return (
     <Flex>
-      <Bold color="violet">{props.name}</Bold>
-      <Text>{props.remarks}</Text>
-      <Text>{props.updated}</Text>
+      <Bold color="violet" className="flex-1 truncate">{props.name}</Bold>
+      <Text className="flex-1 truncate">{props.remarks}</Text>
+      <Text className="flex-1">{props.updated}</Text>
     </Flex>
   );
 }
 
-export interface ItemProps extends CinemaDto {}
+export interface ItemProps extends CinemaDto { }
