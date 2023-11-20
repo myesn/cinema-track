4c2891ad3523bbaeabee6e4635389c8e6f77565b
myesn <myesn@foxmail.com>
2023-11-20 23:33:17 +0800

    refactor: encapsulation useCinema hooks

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,11 @@
 
 import { useEffect, useState } from "react";
 import supabase from "@/supabse";
-import { CinemaDto } from "@/types/cinema.dto";
-import { PostgrestError } from "@supabase/supabase-js";
 import { CinemaUpsertForm } from "@/components/CinemaManage/CinemaUpsertCard";
 import SignIn from "@/components/SignInModal";
 import UserBlock from "@/components/UserBlock";
 import CinemaManage from "@/components/CinemaManage/CinemaManage";
+import { useCinema } from "@/hooks/useCinema";
 
 export default function Home() {
   const [signinVisible, setSigninVisible] = useState(false);
@@ -89,99 +88,4 @@ export default function Home() {
 interface User {
   id: string;
   email: string;
-}
-
-function useCinema() {
-  const [cinemas, setCinemas] = useState<CinemaDto[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [upserting, setUpserting] = useState(false);
-  const [error, setError] = useState<PostgrestError | null>(null);
-  let filteredCinemas: CinemaDto[] = [...cinemas];
-
-  // 立即执行
-  // useEffect(() => {
-  //   (async () => {
-
-  //   })();
-  // }, []);
-
-  // if (props.keyword) {
-  //   filteredCinemas = [
-  //     ...cinemas.filter((x) => x.name.toLowerCase().includes(props.keyword!)),
-  //   ];
-  // }
-
-  async function list() {
-    setLoading(true);
-
-    const { data, error } = await supabase()
-      .from("cinemas")
-      .select()
-      .order("updated_at", { ascending: false });
-
-    if (error) {
-      setError(error);
-    }
-
-    if (!error && data) {
-      const items: CinemaDto[] = data.map((x) => ({
-        id: x.id,
-        name: x.name,
-        remarks: x.remarks ?? "",
-        updated: new Date(x.updated_at).toLocaleDateString(),
-      }));
-
-      setCinemas([...items]);
-    }
-
-    setLoading(false);
-  }
-
-  async function remove(id: number) {
-    if (!id) return;
-
-    setLoading(true);
-
-    const { error } = await supabase().from("cinemas").delete().eq("id", id);
-    if (error) {
-      //setError(error);
-      alert(error.message);
-    } else {
-      setCinemas((items) => [...items.filter((x) => x.id !== id)]);
-    }
-
-    setLoading(false);
-  }
-
-  async function upsert(userId: string, form: CinemaUpsertForm) {
-    setUpserting(true);
-
-    const { error } = await supabase()
-      .from("cinemas")
-      .upsert({
-        id: form.id ?? undefined,
-        name: form.name!,
-        remarks: form.remarks ?? undefined,
-        updated_at: new Date().toISOString(),
-        creator_id: userId,
-      })
-      .select();
-    if (error?.message) {
-      //setError(error);
-      alert(error.message);
-    }
-
-    setUpserting(false);
-  }
-
-  return {
-    loading,
-    upserting,
-    error,
-    cinemas: filteredCinemas,
-
-    list,
-    upsert,
-    remove,
-  };
 }
